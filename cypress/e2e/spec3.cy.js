e796967578af19aae547f8af7ecf5fdc7277ca62
Filipi Dantas <127785380+Filipi0@users.noreply.github.com>
2025-03-26 10:22:21 -0300

    feat: adiciona testes E2E com Cypress e configurações iniciais

---
+++ cypress/e2e/spec3.cy.js
@@ -0,0 +1,20 @@
+describe('Teste E2E: Cadastro de resposta', () => {
+  it('Deve cadastrar uma resposta para a primeira pergunta', () => {
+    cy.visit('http://localhost:3000/');
+
+    cy.get('a[href^="/respostas/?id_pergunta="]').first().click();
+
+    cy.get('textarea[name="resposta"]').type('Resposta automática via Cypress');
+
+    cy.get('input[type="submit"]').click();
+
+    // Confirma que a resposta foi cadastrada
+    cy.contains('Sua resposta foi cadastrada com sucesso.').should('exist');
+
+    // Clica para voltar para a pergunta
+    cy.get('a[href^="/respostas/?id_pergunta="]').first().click();
+
+    // Agora espera a resposta aparecer
+    cy.contains('Resposta automática via Cypress').should('exist');
+  });
+});
